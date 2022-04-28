f289c2cc8579cb8264119abd0f6dabdd6158d19f
Ryan <85891311+astronauLta@users.noreply.github.com>
2022-04-28 13:36:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const socialMediaLinks = {
+  github: 'astronauLta',
+  facebook: '',
+  instagram: '',
+  twitter: '',
+  youtube: ''
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${socialMediaLinks[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
